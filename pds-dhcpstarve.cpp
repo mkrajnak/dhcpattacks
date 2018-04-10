73344f1a4b20294b603d9c49e28bd082d63a3baf
Martin Krajnak <mkrajnak@redhat.com>
2018-04-11 01:05:23 +0200

    args parsed + simple help

--- pds-dhcpstarve.cpp
+++ pds-dhcpstarve.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
 #include <getopt.h>
+#include <cstring>
 
 using namespace std;
 
-int main(int argc, char** argv) {
+void help() {
+  printf("HELP\n");
+}
 
-    int opt;
-    while ((opt = getopt (argc, argv, "i")) != -1)
-    {
-        switch (opt) {
-            case 'i':
-                printf ("Input file: \"%s\"\n", optarg);
-                break;
-            default: perror("Wrong arguments");
-        }
-    }
-    cout << "Hello, World!" << std::endl;
-    return 0;
+void err(string msg, int erno, short show_help){
+  fprintf(stderr, "%s\n", msg.c_str());
+  if (show_help) help();
+  exit(erno);
+}
+
+string checkArgs(int argc, char **argv) {
+  if ((argc != 3 ) || (strcmp(argv[1], "-i") != 0)) {
+    err("Wrong arguments", 1, 0);
+  }
+  return argv[2];
+}
+
+int main(int argc, char** argv) {
+  string interface = checkArgs(argc, argv);
+  return 0;
+
 }
